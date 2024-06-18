e6d7ae988681e484df0c019e0f99032b172a30bb
paulos8deifel <paulos.deifel@gmail.com>
2024-06-18 11:01:07 +0200

    k

--- Hello.R
+++ Hello.R
@@ -1,9 +1,6 @@
-# Hello Funktioniert das Überhaupt?
-# Hier kommt noch mehr test
-# Und nochmal
-# Ein viertes Mal
-# Ein fünftes Mal
-#ein sechstes Malu
+# Einstellung
 install.packages("gitcreds")
 library(gitcreds)
 gitcreds_set()
+
+#Erster Überblick
