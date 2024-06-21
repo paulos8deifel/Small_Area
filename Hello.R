831a0821d94d8dfe483275b758e19089cb91a064
paulos8deifel <paulos.deifel@gmail.com>
2024-06-21 16:59:03 +0200

    buh

--- Hello.R
+++ Hello.R
@@ -12,18 +12,6 @@ library(heaven)
 
 #Paulos Working Directory
 #setwd("/Users/paulo/Documents/Dokumente - MacBook Air (10)/Dokumente/Bamberg/Semester 2/Small Area Estimation/RW_2019-20_DHS_06172024_1312_215365")
-rwanda_1 <- read.delim("RWHR81FL.DAT", sep = " ", header = TRUE)
-View(rwanda_1)
-
-#use rdhs ( -> ~/Library/Caches/rdhs/rdhs.json)
-
-install.packages("devtools")
-devtools::install_github("ropensci/rdhs")
-library(rdhs)
-set_rdhs_config(email = "paulos.deifel@stud.uni-bamberg.de",
-                project = "Small Area
-Estimation Rwanda DHS, 2019-20 - Final Report Data ")
-
-
-rwanda <- dhs_data(countryIds = "RW", surveyYearStart = 2019, surveyYearEnd = 2020)
+rwanda <- read.csv("RWHR81FL.DAT", header = FALSE, sep = " ")
 View(rwanda)
+length(rwanda)
